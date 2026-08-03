d496d6a8fe1b8bc56a9808fd4178a6a3367c6de5
shayp9797 <joshperera24@gmail.com>
2026-08-03 15:23:47 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE='spideyvault-v2.9.4';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./catalogue.js','./manifest.webmanifest','./icon.svg','./images/spider-gwen-405.jpeg','./images/spider-gwen-153-unhooded.jpeg'];
+const CACHE='spideyvault-v2.9.5';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./catalogue.js','./manifest.webmanifest','./icon.svg',
+  './images/ins-399-negative-suit.png',
+  './images/ins-397-miles.png',
+  './images/ins-334-advanced-suit.png','./images/spider-gwen-405.jpeg','./images/spider-gwen-153-unhooded.jpeg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
